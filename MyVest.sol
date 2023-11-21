4982d091d62639051bb5372ca20f0cf8dc82f56a
ScottieMadura94 <101625029+ScottieMadura94@users.noreply.github.com>
2023-11-21 07:49:38 +0200

    Update MyVest.sol

--- MyVest.sol
+++ MyVest.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TokenVesting {
+    address public beneficiary;
+    uint256 public cliff;
+    uint256 public start;
+    uint256 public duration;
+    uint256 public released;
+    bool public revocable;
+    bool public revoked;
+
+    IERC20 public token;
+
+    event TokensReleased(uint256 amount);
+    event VestingRevoked();
+
+    modifier onlyBeneficiary() {
+        require(msg.sender == beneficiary, "Only the beneficiary can call this function");
+        _;
+    }
+
+    constructor(
+        address _beneficiary,
+        uint256 _cliff,
+        uint256 _start,
+        uint256 _duration,
+        bool _revocable,
+        address _token
+    ) {
+        require(_beneficiary != address(0), "Invalid beneficiary address");
+        require(_cliff <= _duration, "Cliff period must be less than or equal to duration");
+
+        beneficiary = _beneficiary;
+        cliff = _start + _cliff;
+        start = _start;
+        duration = _duration;
+        revocable = _revocable;
+        token = IERC20(_token);
+    }
+
+}
